e9a6f78a3bd6360343da2ca2cb9a307d6faa0c78
codename-24 <vijay2495@gmail.com>
2022-06-02 13:15:49 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+
+mongoose.connect('mongodb+srv://cd_24:codename24@cluster0.2r3aa.mongodb.net/Messenger?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology:true},()=>{
+    console.log("💾 Connected to database")
+})
+
+
+app.use(cors());
+app.use("/images",express.static(path.join(__dirname,"public/images")))
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+      return res.status(200).json("File uploded successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
+app.use("/api/posts",postRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
+
+
+
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("🚀 Server running ")
+})
---
+++ routes/users.js
@@ -0,0 +1,125 @@
+const User = require("../models/User")
+const router = require("express").Router();
+const bcrypt = require("bcrypt")
+
+//update
+router.put("/:id",async (req,res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (error) {
+                return res.status(500).json(error)
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body,
+            });
+            res.status(200).json("Account has been updated")
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    }
+    else{
+        return res.status(403).json("You can update only your account")
+    }
+})
+
+//delete
+router.delete("/:id",async (req,res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted")
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    }
+    else{
+        return res.status(403).json("You can delete only your account")
+    }
+})
+
+
+//get user
+router.get("/", async(req,res)=>{
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+        const user = userId ? await User.findById(userId) : await User.findOne({username: username });
+        const {password,updatedAt, ...other} = user._doc
+        res.status(200).json(other);
+    } catch (error) {
+        res.status(500).json(error)
+        
+    }
+})
+
+//get friends
+router.get("/friends/:userId", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      const friends = await Promise.all(
+        user.following.map((friendId) => {
+          return User.findById(friendId);
+        })
+      );
+      let friendList = [];
+      friends.map((friend) => {
+        const { _id, username, profilePicture } = friend;
+        friendList.push({ _id, username, profilePicture });
+      });
+      res.status(200).json(friendList)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//follow user
+router.put("/:id/follow", async(req,res)=>{
+  if(req.body.userId !== req.params.id){
+      try {
+          const user = await User.findById(req.params.id);
+          const currentUser = await User.findById(req.body.userId);
+          if(!user.followers.includes(req.body.userId)){
+              await user.updateOne({$push:{followers: req.body.userId}});
+              await currentUser.updateOne({$push:{following: req.params.id}});
+              res.status(200).json("You are following this user now")
+          }else{
+              res.status(403).json("You are already following this user");
+          }
+      } catch (error) {
+          res.status(500).json(error);
+      }
+  }
+  else{
+      res.status(403).json("umm?")
+  }
+})
+
+//unfollow user
+
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        const currentUser = await User.findById(req.body.userId);
+        if (user.followers.includes(req.body.userId)) {
+          await user.updateOne({ $pull: { followers: req.body.userId } });
+          await currentUser.updateOne({ $pull: { following: req.params.id } });
+          res.status(200).json("user has been unfollowed");
+        } else {
+          res.status(403).json("you don't follow this user");
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("???");
+    }
+  });
+
+
+module.exports = router;
